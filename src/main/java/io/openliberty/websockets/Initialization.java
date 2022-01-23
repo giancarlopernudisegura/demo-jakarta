b7237f3e6da02a8e33a4c74b459c3609493d8571
Lidia Ataupillco <ataupill@ualberta.ca>
2022-01-22 17:19:04 -0700

    websocket working

---
+++ src/main/java/io/openliberty/websockets/Initialization.java
@@ -0,0 +1,40 @@
+package io.openliberty.websockets;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import jakarta.websocket.server.ServerEndpoint;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.OnError;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnClose;
+import jakarta.websocket.Session;
+
+
+// Followed tutorial from https://www.baeldung.com/java-websockets
+@ServerEndpoint(value = "/infos")
+public class Initialization {
+	private static Session session;	
+	
+	@OnOpen
+	public void OnOpen(Session session) throws IOException {
+		this.session = session;
+		System.out.println("Websocket opened: " + session.getId().toString());
+	}
+	
+	@OnMessage
+    public void onMessage(String message, Session session) {
+        System.out.println("Program requested " + message + " using " + session.getId());
+        session.getAsyncRemote().sendText("Lucky says hi");
+    }
+ 
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        System.out.println("There was an error WebSocket error for " + session.getId() + " " + throwable.getMessage());
+    }
+ 
+    @OnClose
+    public void onClose(Session session) {
+        System.out.println("WebSocket closed for " + session.getId());
+    }
+}
