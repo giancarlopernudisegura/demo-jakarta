ac14c1aa3ae80fc209064baed6820bb9376fdd09
Lidia Ataupillco <ataupill@ualberta.ca>
2022-02-03 15:07:14 -0700

    show issues

--- src/main/java/io/openliberty/websockets/Initialization.java
+++ src/main/java/io/openliberty/websockets/Initialization.java
@@ -27,12 +27,18 @@ public class Initialization {
         System.out.println("Program requested " + message + " using " + session.getId());
         session.getAsyncRemote().sendText("Lucky says hi");
     }
- 
+
+    @OnMessage
+    public void notValidMessage(String message, Session session) {
+        System.out.println("Program requested " + message + " using " + session.getId());
+        session.getAsyncRemote().sendText("I am never shown");
+    }
+
     @OnError
-    public void onError(Session session, Throwable throwable) {
+    public void onError(Session session, Throwable throwable, String test) {
         System.out.println("There was an error WebSocket error for " + session.getId() + " " + throwable.getMessage());
     }
- 
+
     @OnClose
     public void onClose(Session session) {
         System.out.println("WebSocket closed for " + session.getId());
